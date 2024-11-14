734d39c1aaf39a27d1a9c3ef0458174298e74a33
dav1dmc7 <david.marcobo@gmail.com>
2024-11-14 21:43:06 +0100

    Descripción de los cambios

--- functions/process-form.js
+++ functions/process-form.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const secretKey = process.env.RECAPTCHA_SECRET_KEY;
-  const supabaseUrl = 'https://TU_SUPABASE_URL';
+  const supabaseUrl = 'https://jnkluabtktatvtsbfamn.supabase.co';
   const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
   const { createClient } = require('@supabase/supabase-js');
   const supabase = createClient(supabaseUrl, supabaseKey);
