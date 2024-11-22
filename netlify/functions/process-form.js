fe970c6425e569577d8d1c4db61e58292c3dce8d
dav1dmc7 <david.marcobo@gmail.com>
2024-11-22 22:11:23 +0100

    Corrección de dependencias y configuración de funciones

--- netlify/functions/process-form.js
+++ netlify/functions/process-form.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const nodemailer = require('nodemailer');
 
 exports.handler = async (event) => {
   try {
     const data = JSON.parse(event.body);
     const { nombre, email, mensaje } = data;
+
+    console.log('Datos recibidos:', data); // Imprimir datos recibidos
 
     // Configurar Nodemailer para enviar correo con Gmail
     const transporter = nodemailer.createTransport({
@@ -12,6 +16,15 @@ exports.handler = async (event) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+    });
+
+    // Verificar conexión antes de enviar el correo
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.error('Error de conexión con el servidor de correo:', error);
+      } else {
+        console.log('Servidor listo para enviar correos');
+      }
     });
 
     const mailOptions = {
@@ -32,14 +45,14 @@ exports.handler = async (event) => {
       body: JSON.stringify({ message: 'Correo enviado correctamente' }),
     };
   } catch (error) {
-    console.error('Error al enviar el correo:', error.message, error.stack);
+    console.error('Error al enviar el correo:', error);
     return {
       statusCode: 500,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
       },
-      body: JSON.stringify({ message: `Error al enviar el correo: ${error.message}` }),
+      body: JSON.stringify({ message: 'Error al enviar el correo' }),
     };
   }
 };
